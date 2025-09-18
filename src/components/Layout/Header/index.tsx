38262da4cccd09a7490aa26c0bca80804dec491d
Anano Topuria <anano.topuriaa@gmail.com>
2025-09-18 22:40:42 +0400

    feat: add header; integrate Tailwind CSS

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -1,5 +1,16 @@
+import LogoImg from "./../../../assets/images/logo.png";
+import { FaUser } from "react-icons/fa6";
+
 function Header() {
-  return <header>this is header</header>;
+  return (
+    <header className="flex justify-between px-[10rem] py-[2.8rem]">
+      <img className="w-[18rem]" src={LogoImg} alt="logo" />
+      <button className="flex justify-center items-center gap-4 font-400 text-[1.2rem]">
+        <FaUser />
+        Log in
+      </button>
+    </header>
+  );
 }
 
 export default Header;
